a8ad4ef2b27542e12cf1baf4b7311ab2318b1d10
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-08 12:10:45 +0100

    Factorization implementation with C

---
+++ factors.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * factorize - function that prints out the
+ * two smallest factors of an integer
+ * @n: integer to be factorized.
+*/
+
+void factorize(unsigned long long int n)
+{
+for (unsigned long long int i = 2; i <= n; i++)
+{
+if (n % i == 0)
+{
+printf("%llu=%llu*%llu\n", n, n / i, i);
+break;
+}
+}
+return;
+}
+
+/**
+ * main - our program's entry point.
+ * @argc: number of commandling arguments including the filename
+ * @argv: array of character pointers listing all the arguments
+ * Return: Always 0.
+*/
+
+int main(int argc, char** argv)
+{
+if (argc > 2 || argc < 2)
+    fprintf(stderr, "Usage: ./factors <file>\n");
+
+
+FILE *fac_file;
+
+fac_file = fopen(argv[1], "r");
+
+unsigned long long int finput = 0;
+
+while (fscanf(fac_file, "%llu", &finput) != EOF)
+{
+factorize(finput);
+}
+
+// factorize(1718944270642558716715);
+fclose(fac_file);
+
+return (0);
+}
